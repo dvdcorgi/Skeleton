2fe96def02e30b6fd1dbf1012022b4f690a3d562
dadat <dvdbarredo@gmail.com>
2017-07-27 13:53:45 +0800

    Add Model, Update config sections, Update Service

---
+++ SkelService/IServiceSkeleton.cs
@@ -0,0 +1,32 @@
+﻿using SkelService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SkelService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceSkeleton
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        bool AddItem(tblSkeleton tSkeleton);
+
+        [OperationContract]
+        List<tblSkeleton> GetItems(string itemName);
+
+        [OperationContract]
+        bool UpdateItem(tblSkeleton tSkeleton);
+
+    }
+
+    
+}
---
+++ SkelService/ServiceSkeleton.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SkelService.Model;
+
+namespace SkelService
+{
+
+    public class Service1 : IServiceSkeleton
+    {
+        dbSkeletonEntities db = new dbSkeletonEntities();
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public bool AddItem(tblSkeleton tSkeleton)
+        {
+            try
+            {
+                tblSkeleton t = new tblSkeleton();
+                t.Name = tSkeleton.Name;
+                t.Code = tSkeleton.Code;
+                db.tblSkeletons.Add(t);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AddItem error: " + ex.Message);
+                return false;
+            }
+        }
+
+        public List<tblSkeleton> GetItems(string itemName)
+        {
+            List<tblSkeleton> listSkeleton = (from a in db.tblSkeletons
+                                        where a.Name.Contains(itemName)
+                                        select a).ToList();
+            return listSkeleton;
+        }
+
+        public bool UpdateItem(tblSkeleton tSkeleton)
+        {
+            try
+            {
+                tblSkeleton t = (from a in db.tblSkeletons
+                                 where a.id == tSkeleton.id
+                                 select a).FirstOrDefault();
+                t.Name = tSkeleton.Name;
+                t.Code = tSkeleton.Code;
+                db.tblSkeletons.Add(t);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UpdateItem error: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
